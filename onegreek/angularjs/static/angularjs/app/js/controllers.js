0fa9cdd35cbfefe9a8ca6c08f8c1278c42f4dbee
goldhand <will@webpowerlabs.com>
2013-11-07 14:14:44 -0700

    angularjs: set up event form with date picker widget

--- onegreek/angularjs/static/angularjs/app/js/controllers.js
+++ onegreek/angularjs/static/angularjs/app/js/controllers.js
@@ -143,10 +143,71 @@ myApp.controller('PopoverDemoCtrl', ['$scope', function ($scope) {
 }]);
 
 
-myApp.controller('MyFormCtrl', ['$scope', '$http', function ($scope, $http) {
+
+myApp.controller('DatepickerDemoCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.showWeeks = true;
+  $scope.toggleWeeks = function () {
+    $scope.showWeeks = ! $scope.showWeeks;
+  };
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = ( $scope.minDate ) ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function() {
+    $timeout(function() {
+      $scope.opened = true;
+    });
+  };
+
+  $scope.dateOptions = {
+    'year-format': "'yyyy'",
+    'month-format': "'mm'",
+    'starting-day': 1
+  };
+}]);
+
+myApp.controller('MyFormCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
     $scope.submit = function() {
         $http.post('/api/events/', $scope.events).success(function(out_data) {
             // do something
         });
-    }
+    };
+
+    $scope.openStart = function() {
+        $timeout(function() {
+            $scope.openedStart = true;
+        });
+    };
+    $scope.openEnd = function() {
+        $timeout(function() {
+            $scope.openedEnd = true;
+        });
+    };
+
+
+    $scope.dateOptions = {
+        'year-format': "'yyyy'",
+        'month-format': "'mm'",
+        'starting-day': 1
+    };
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+
+    $scope.ismeridian = true;
 }]);
--- onegreek/events/forms.py
+++ onegreek/events/forms.py
@@ -9,9 +9,31 @@ from djangular.forms.angular_model import NgModelFormMixin
 from .models import Event
 
 
+def date_time_widget(ng_model, ng_label):
+    return \
+    '<div class="form-inline row">' \
+    '<div class="span3" style="padding-top: 8px;">' \
+    '<label class="control-label">%s</label>' \
+    '<div class="controls">' \
+    '<input class="span2" type="text" datepicker-popup="yyyy-MM-dd" ng-model="%s" is-open="opened%s" min="minDate" max="\'2015-06-22\'" datepicker-options="dateOptions" ng-required="false" />' \
+    '<button class="btn" ng-click="open%s()"><i class="icon-calendar"></i></button>' \
+    '</div>' \
+    '</div>' \
+    '<div class="controls">' \
+    '<div ng-model="%s" ng-change="changed()" class="span3">' \
+    '<timepicker hour-step="hstep" minute-step="mstep" show-meridian="ismeridian"></timepicker>' \
+    '</div>' \
+    '</div>' \
+    '</div>' \
+    % (ng_label, ng_model, ng_label, ng_label, ng_model)
+
 class EventForm(NgModelFormMixin, forms.ModelForm):
     class Meta:
         model = Event
+        fields = [
+            'title', 'description', 'viewers', 'enable_comments',
+            'owner', 'slug',
+        ]
         widgets = {
             'owner': forms.HiddenInput(),
             'slug': forms.HiddenInput(),
@@ -26,5 +48,13 @@ class EventForm(NgModelFormMixin, forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.form_class = 'form-horizontal'
-        #self.helper.layout = Layout(
-        #    Fieldset(
+        self.helper.layout = Layout(
+            Div(
+                HTML(date_time_widget('events.start', 'Start')),
+                HTML(date_time_widget('events.end', 'End')),
+                'title',
+                'description',
+                'viewers',
+                'enable_comments'
+            )
+        )
