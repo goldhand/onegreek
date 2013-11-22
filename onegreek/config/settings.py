4357c2339f758d7c8f7dc9b1fce83ed098ac3e63
goldhand <will@webpowerlabs.com>
2013-11-21 18:16:10 -0700

    Settings configured for deployment *order of apps is corect

--- onegreek/config/settings.py
+++ onegreek/config/settings.py
@@ -472,20 +472,7 @@ class Production(Common):
 
     ########## CACHING
     # Only do this here because thanks to django-pylibmc-sasl and pylibmc memcacheify is painful to install on windows.
-    #CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
-
-    os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '').replace(',', ';')
-    os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
-    os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
-
-    CACHES = {
-        'default': {
-            'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-            'TIMEOUT': 500,
-            'BINARY': True,
-            'OPTIONS': { 'tcp_nodelay': True }
-        }
-    }
+    CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
     ########## END CACHING
 
 
