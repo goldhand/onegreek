bd454bdc33886115b27e2d935df3f974abac878b
Will Farley <will@django.nu>
2013-12-11 23:38:28 -0700

    adds image support lesscss designs user profile enhancements

---
+++ onegreek/imagestore/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+
+from .models import RestComment
+from events.models import Event
+from chapters.models import Chapter
+
+
+class ContentObjectRelatedField(serializers.RelatedField):
+    """
+    A custom field to use for the `content_object` generic relationship.
+    """
+
+    def to_native(self, value):
+        """
+        Serialize content objects to a simple textual representation.
+        """
+        if isinstance(value, Event):
+            return value.get_absolute_url()
+        if isinstance(value, Chapter):
+            return value.get_absolute_url()
+        #elif isinstance(value, ''):
+        #    return 'Note: ' + value.text
+        raise Exception('Unexpected type of content object')
+
+
+class RestCommentSerializer(serializers.HyperlinkedModelSerializer):
+    #viewers = serializers.PrimaryKeyRelatedField(many=True)
+    #content_object = ContentObjectRelatedField()
+    content_type = serializers.Field(source='content_type')
+    name = serializers.Field(source='name')
+    profile_image_url = serializers.SerializerMethodField('get_profile_image_url')
+    #site = serializers.PrimaryKeyRelatedField()
+    content_object_url = serializers.Field(source='get_content_object_url')
+    content_object = serializers.Field(source='content_object')
+
+    class Meta:
+        model = RestComment
+        fields = [
+            'user',
+            'name',
+            'comment',
+            'url',
+            'profile_image_url',
+            'submit_date',
+            'content_type',
+            'object_pk',
+            'content_object_url',
+            'content_object',
+            #'timestamp',
+            #'security_hash',
+        ]
+        read_only_fields = [
+            'submit_date',
+            #'content_type',
+            #'object_pk',
+            #'content_object',
+            #'timestamp',
+            #'security_hash',
+        ]
+
+    def get_profile_image_url(self, obj):
+        if obj:
+            return obj.user.profile_image_url()
+        else:
+            return None
---
+++ onegreek/users/static/users/app/js/app-profile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var userApp = angular.module('userApp', [
+    'ui.bootstrap',
+    'ngCookies',
+    'ngRoute',
+    'ngDragDrop',
+    'userControllers',
+    'userServices',
+    'userDirectives',
+    //'groupControllers',
+    'groupServices'
+    //'myApp.filters',
+    //'myApp.services',
+    //'myApp.directives',
+    //'myApp.controllers'
+],
+    function($interpolateProvider) {
+        $interpolateProvider.startSymbol("{[{");
+        $interpolateProvider.endSymbol("}]}");
+    }
+);
+
+userApp.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $routeProvider.when('/users', {
+        templateUrl: 'list.html',
+        controller: 'UserListCtrl',
+        resolve: {
+            users: function(UserService) {
+                return UserService.list();
+            },
+            groups: function(GroupService) {
+                return GroupService.list();
+            }
+        }
+    });
+    $routeProvider.when('/users/filter/:query/:queryId', {
+        templateUrl: 'list.html',
+        controller: 'UserListCtrl',
+        resolve: {
+            users: function($route, UserService) {
+                var q = $route.current.params.query;
+                var qId = $route.current.params.queryId;
+                return UserService.filter(q, qId);
+            }
+        }
+    });
+    $routeProvider.when('/users/:userId', {
+        templateUrl: 'detail.html',
+        controller: 'UserDetailCtrl',
+        resolve: {
+            user: function($route, UserService) {
+                var userId = $route.current.params.userId;
+                return UserService.get(userId);
+            }
+        }
+
+    });
+    $routeProvider.when('/events/calendar', {
+        templateUrl: 'http://localhost:8000/events/calendar/2013/11/'
+    });
+    $routeProvider.when('/events/calendar/rsvp', {
+        templateUrl: 'http://localhost:8000/events/calendar/2013/11/?rsvp=true'
+    });
+    $routeProvider.otherwise({redirectTo: '/users'});
+}]);
