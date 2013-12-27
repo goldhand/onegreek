6c1741628c29c78b721e428c8321ae14527ed9bf
Will Farley <will@django.nu>
2013-12-26 18:02:14 -0700

    Fixes priorities 1 - 5

--- onegreek/config/settings.py
+++ onegreek/config/settings.py
@@ -290,7 +290,7 @@ class Common(Configuration):
 
     ########## Custom user app defaults
     # Select the correct user model
-    AUTH_USER_MODEL = "users.User"
+    #AUTH_USER_MODEL = "users.User"
     LOGIN_REDIRECT_URL = "users:redirect"
     ########## END Custom user app defaults
 
